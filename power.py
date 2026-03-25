247829f9f0b2c3174c96083e57d76eea4f4d1b42
John Christensen <netbard@gmail.com>
2026-03-25 16:53:12 -0400

    chapter-11-practice-7

--- power.py
+++ power.py
@@ -9,3 +9,6 @@ def calc_max_average_power(voltage_thevenin, impedance_thevenin):
 
 def calc_max_average_power_resistive(current_thevenin, load_resistance):
     return 0.5 * get_magnitude(current_thevenin) ** 2 * load_resistance
+
+def calc_power(current_rms, resistance):
+    return current_rms ** 2 * resistance
